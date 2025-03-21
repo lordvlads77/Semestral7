3d662046499abf16fc246b57c578111fb5816453
y1z <whyis2cool@gmail.com>
2025-03-21 15:56:40 -0600

    Hacer enemy que aventa proyectiles

--- Assets/Scripts/Entity/ThrowingEnemy.cs
+++ Assets/Scripts/Entity/ThrowingEnemy.cs
@@ -16,6 +16,9 @@ namespace Entity
 
         private float realHealth = 10f;
 
+        [Header("Throwing object")]
+        [SerializeField] GameObject prefab;
+
         [Header("Enemy properties")]
 
         [Range(0f, 20f)]
@@ -38,8 +41,14 @@ namespace Entity
             }
         }
 
-        [SerializeField] float attackCooldown = .4f;
+        [SerializeField] float attackCooldown = 1.4f;
+
+        [SerializeField] float attackRange = 5.0f;
+
         [SerializeField] float timeInsdeAttackRange = 0f;
+
+        [SerializeField] Projectile PreFab;
+        [SerializeField] List<Projectile> Spawned;
 
         void Start()
         {
@@ -49,12 +58,69 @@ namespace Entity
             EDebug.Assert(temp_player != null, "could not find player character", this);
             player = temp_player.GetComponent<LivingEntity>();
             agent.speed = speed;
+
+            gameState = GameStates.Playing;
             SetHealth(health);
         }
 
         void Update()
         {
             if (gameState != GameStates.Playing) { return; }
+
+
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+            EDebug.Log("Distance from Enemy" + distance);
+            if (distance < attackRange)
+            {
+                timeInsdeAttackRange += Time.deltaTime;
+            }
+            else
+            {
+                timeInsdeAttackRange = 0;
+            }
+
+            if (timeInsdeAttackRange >= attackCooldown)
+            {
+                Attack();
+                timeInsdeAttackRange = 0.0f;
+            }
+
+        }
+
+        public void Attack()
+        {
+            Projectile final_projectile = Recycle();
+
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            
+            if (final_projectile == null) {
+                Projectile go = Instantiate(PreFab, transform.position + (direction * 2.0f), Quaternion.identity);
+                go.setDestination(player.transform.position);
+                Spawned.Add(go);
+                return;
+            }
+
+            final_projectile.gameObject.SetActive(true);
+            final_projectile.transform.position = transform.position + (direction * 2.0f);
+            final_projectile.setDestination(player.transform.position);
+        }
+
+        public Projectile Recycle()
+        {
+            Projectile result = null;
+            foreach (Projectile p in Spawned)
+            {
+                if (!p.gameObject.activeInHierarchy)
+                {
+                    result = p; 
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public void Prepare(Projectile pro)
+        {
 
         }
     }
