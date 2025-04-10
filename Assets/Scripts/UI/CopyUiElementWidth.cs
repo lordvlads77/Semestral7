f4f385dbf3432dfa219f2d049dc9455d9806e943
y1z <whyis2cool@gmail.com>
2025-04-10 13:32:24 -0600

    BUGFIX : Ahora si cambia de tamaño el elemento

--- Assets/Scripts/UI/CopyUiElementWidth.cs
+++ Assets/Scripts/UI/CopyUiElementWidth.cs
@@ -2,36 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CopyUiElementWidth : MonoBehaviour
+/// <summary>
+/// Hace que el elemento con el script copie la anchura de otro elemento
+/// <note>
+/// Si no se asigna el otherUiRectTransform el script en efecto hace nada
+/// </note>
+/// </summary>
+public sealed class CopyUiElementWidth : MonoBehaviour
 {
-    [SerializeField] private RectTransform selfUiRectTransform;
+    private RectTransform selfUiRectTransform;
 
+    [Tooltip("Si no se asigna esta variable el script hace nada")]
     [SerializeField] private RectTransform otherUiRectTransform;
 
+    private bool hasOtherUIRectTransformAssigned;
 
     private void Awake()
     {
+        hasOtherUIRectTransformAssigned = false;
+
         if (selfUiRectTransform == null)
         {
             selfUiRectTransform = GetComponent<RectTransform>();
         }
         Debug.Assert(selfUiRectTransform != null, $"The Script Need a {nameof(RectTransform)} to work ", this);
-        Debug.Assert(otherUiRectTransform != null, "<color=orange>The variable otherUiRectTransform is null fix that</color>", this);
+        hasOtherUIRectTransformAssigned = otherUiRectTransform != null;
+        //Debug.Assert(otherUiRectTransform != null, "<color=orange>The variable otherUiRectTransform is null fix that</color>", this);
     }
 
     private void FixedUpdate()
     {
+        if (!hasOtherUIRectTransformAssigned) { return; }
+
         float selfWidth = selfUiRectTransform.rect.width;
         float otherWidth = otherUiRectTransform.rect.width;
 
         if (selfWidth < otherWidth || selfWidth > otherWidth)
         {
             Rect selfRect = selfUiRectTransform.rect;
-
-            selfUiRectTransform.rect.Set(selfRect.x,
-                selfRect.y,
-                selfRect.height,
-                otherUiRectTransform.rect.width);
+            //selfRect.size = new Vector2(otherUiRectTransform.rect.width, selfRect.height);
+            selfUiRectTransform.sizeDelta = new Vector2(otherUiRectTransform.rect.width, selfRect.height);
 
         }
 
