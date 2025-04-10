f74cb4556d54793feefb8ce70622e775824e802a
y1z <whyis2cool@gmail.com>
2025-04-10 11:38:32 -0600

    Cambios a ButtonInfo.cs: se puede saber cuando fue la ultima vez que se hizo click sobre un button
    * tambien tien funciones para saber si paso 5 segundos desde el ultimo click 1 segundo desde el ultimo click y el 'currentTimeSinceLastClick' te dice cuanto tiempo paso desde el ultimo click

--- Assets/Scripts/UI/ButtonInfo.cs
+++ Assets/Scripts/UI/ButtonInfo.cs
@@ -5,12 +5,24 @@ using UnityEngine.EventSystems;
 
 namespace UI
 {
-    public class ButtonInfo : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+    /// <summary>
+    /// Esta clase solo es para extarer informacion de el 
+    /// Boton de unity (no el de textMeshPro)
+    /// </summary>
+    public sealed class ButtonInfo : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
 
         [SerializeField] public UnityEngine.UI.Button button;
 
         public bool isPointerOverButton { get; private set; } = false;
+
+        public bool hasBeenClickedEver { get; private set; } = false;
+
+        private const float fiveSeconds = 5.0f;
+
+        private const float oneSecond = 1.0f;
+
+        public float currentTimeSinceLastClick { get; private set; } = 0.0f;
 
         private void Awake()
         {
@@ -18,9 +30,16 @@ namespace UI
             EDebug.Assert(button != null, "Attach this script to a unity button");
         }
 
+        private void Update()
+        {
+            if (!hasBeenClickedEver) { return; }
+
+            currentTimeSinceLastClick += Time.deltaTime;
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
-
+            hasBeenClickedEver = true;
         }
 
         public void OnPointerEnter(PointerEventData eventData)
@@ -33,6 +52,16 @@ namespace UI
         {
 
             isPointerOverButton = false;
+        }
+
+        public bool hasButtonBeenClickFiveSecondsAgo()
+        {
+            return currentTimeSinceLastClick >= fiveSeconds;
+        }
+
+        public bool hasButtonBeenClickOneSecondsAgo()
+        {
+            return currentTimeSinceLastClick >= oneSecond;
         }
 
     }
