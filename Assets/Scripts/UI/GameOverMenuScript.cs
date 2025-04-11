02c164189494cdd94c2f7750639612883402e853
y1z <whyis2cool@gmail.com>
2025-04-11 13:03:53 -0600

    GameOverMenuScript.cs es para la pantalla de game over

---
+++ Assets/Scripts/UI/GameOverMenuScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class GameOverMenuScript : MonoBehaviour
+    {
+        public void Retry()
+        {
+            // recarga la ecena actuial
+            // si no funciona ve a File -> Builds Settings
+            // y agrega la Ecena actual
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+        public void Options()
+        {
+            EDebug.Log($"<color=orange>{nameof(this.Options)} is not implemented yet </color>");
+        }
+
+        public void Menu()
+        {
+
+            EDebug.Log($"<color=orange>{nameof(this.Menu)} is not implemented yet </color>");
+        }
+
+    }
+
+}
