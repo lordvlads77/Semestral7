2242d31a13d34d5d2b773cfff7c87ff876c51c09
y1z <whyis2cool@gmail.com>
2025-04-27 18:46:56 -0600

    TestLoading.cs : Script para probar el script Loading.cs

---
+++ Assets/Scripts/Test/TestLoading.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoading : MonoBehaviour
+{
+    [Tooltip("☑ = load the level selected\n☐ = DON'T load the level selected")]
+    public bool shouldLoad = false;
+
+    [Tooltip("Attach the Loading script here for this to work")]
+    [SerializeField] private Loading loadingScript;
+
+    void Start()
+    {
+        Debug.Assert(loadingScript != null,$"Attach script type of {nameof(Loading)} for this to work",this);
+    }
+
+    void Update()
+    {
+
+        if (shouldLoad)
+        {
+            shouldLoad = false;
+            loadingScript.LoadScene();
+        }
+        
+    }
+}
