d809b63f21c3833f7c6357ae939c576ebd4744ea
y1z <whyis2cool@gmail.com>
2025-04-30 14:03:35 -0600

    TestLoading.cs : Es un script para probar cargar entre ecenas

--- Assets/Scripts/Test/TestLoading.cs
+++ Assets/Scripts/Test/TestLoading.cs
@@ -1,19 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
-public class TestLoading : MonoBehaviour
+public sealed class TestLoading : MonoBehaviour
 {
     [Tooltip("☑ = load the level selected\n☐ = DON'T load the level selected")]
     public bool shouldLoad = false;
 
-    [Tooltip("Attach the Loading script here for this to work")]
-    [SerializeField] private Loading loadingScript;
 
-    void Start()
-    {
-        Debug.Assert(loadingScript != null,$"Attach script type of {nameof(Loading)} for this to work",this);
-    }
+    [SerializeField] private string sceneNameToLoad = "";
+    [SerializeField] private int sceneIndexToLoad = -1337;
 
     void Update()
     {
@@ -21,7 +18,7 @@ public class TestLoading : MonoBehaviour
         if (shouldLoad)
         {
             shouldLoad = false;
-            loadingScript.LoadScene();
+            LoadingManager.Instance.LoadSceneByName(sceneNameToLoad);
         }
         
     }
