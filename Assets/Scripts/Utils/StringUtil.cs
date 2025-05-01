4dd8714af1462a56602b27eb1e7c14daf76ff8de
y1z <whyis2cool@gmail.com>
2025-05-01 15:10:03 -0600

    StringUtil.cs : es una clase que agrega color y size, Tags al texto

---
+++ Assets/Scripts/Utils/StringUtil.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+namespace Utils
+{
+    public static class StringUtil
+    {
+        public static string addSizeTagToString(string input, int size)
+        {
+            string str_size = size.ToString();
+
+            return $"<size={str_size}> {input} </size>";
+        }
+
+        public static string addColorToString(string input, Color color)
+        {
+            string color_str = ColorUtility.ToHtmlStringRGBA(color);
+
+            return $"<color=#{color_str}> {input} </color>";
+        }
+    }
+}
