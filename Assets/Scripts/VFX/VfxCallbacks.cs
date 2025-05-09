17944a4a96179243c84d374a1d9eae3cb5c1452e
Eva Luna Álvarez <zap363@amerike.edu.mx>
2025-05-08 21:12:20 -0600

    Did VFXScreenCallingScript | Put some VFX Fire on the campp

--- Assets/Scripts/VFX/VfxCallbacks.cs
+++ Assets/Scripts/VFX/VfxCallbacks.cs
@@ -8,9 +8,23 @@ public class VfxCallbacks : MonoBehaviour
     public static VfxCallbacks Instance { get; private set; }
     [SerializeField] private GameObject _ImpactHitvfx = default;
     [SerializeField] private Transform _impactHitvfxtRTransform = default;
+    [SerializeField] private GameObject _bloodVFX = default;
+    [SerializeField] private Transform _bloodVfxTransform = default;
+    [SerializeField] private GameObject _groundImpactVFX = default;
+    [SerializeField] private Transform _groundImpactVfxTransform = default;
 
     public void ImpactHitVFX()
     {
         Instantiate(_ImpactHitvfx, _impactHitvfxtRTransform.position, _impactHitvfxtRTransform.rotation);
+    }
+
+    public void bloodVFX()
+    {
+        Instantiate(_ImpactHitvfx, _bloodVfxTransform.position, _bloodVfxTransform.rotation);
+    }
+    
+    public void GroundImpactVFX()
+    {
+        Instantiate(_groundImpactVFX, _groundImpactVfxTransform.position, _groundImpactVfxTransform.rotation);
     }
 }
