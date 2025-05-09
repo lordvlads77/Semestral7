a81ff3c9bb17fac35eafccfa7bc302018f121417
y1z <whyis2cool@gmail.com>
2025-05-09 11:33:37 -0600

    Todo lo que hice en el tutorial level

---
+++ Assets/Scripts/Utils/EndLevelPoint.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Utils
+{
+    [RequireComponent(typeof(BoxcastCommand))]
+    [RequireComponent(typeof(Rigidbody))]
+    public sealed class EndLevelPoint : MonoBehaviour
+    {
+
+        public string SceneName = "Scenes/Nuevo Menu";
+        private Rigidbody rigidbody;
+
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            rigidbody.useGravity = false;
+            rigidbody.isKinematic = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            EDebug.Log($"<color=red>{nameof(OnTriggerEnter)}</color>", this);
+            if (other.CompareTag("Player"))
+            {
+                LoadingManager.Instance.LoadSceneByName(SceneName);
+            }
+            else
+            {
+
+                EDebug.Log($"<color=red> Tag =|{other.tag}|</color>", this);
+            }
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            EDebug.Log($"<color=red>{nameof(OnCollisionEnter)}</color>", this);
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                LoadingManager.Instance.LoadSceneByName(SceneName);
+            }
+
+        }
+
+    }
+}
+
+
