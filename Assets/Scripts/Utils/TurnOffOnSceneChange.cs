034924135843befe293014d8bba53893df8ffdf9
y1z <whyis2cool@gmail.com>
2025-05-15 16:00:17 -0600

    TurnOffOnSceneChange.cs : aún no esta implementada

---
+++ Assets/Scripts/Utils/TurnOffOnSceneChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    // TODO : IMPLEMENTAR ESTA CLASE
+    public class TurnOffOnSceneChange : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
