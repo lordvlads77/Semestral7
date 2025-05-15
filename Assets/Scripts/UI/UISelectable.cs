b6d98c285e406934562845d6f8c4f7b730116627
y1z <whyis2cool@gmail.com>
2025-05-15 15:54:29 -0600

    UISelectable.cs : ahora se puede saber si tiene un blockyslider o textSwitcher el UISelectable

--- Assets/Scripts/UI/UISelectable.cs
+++ Assets/Scripts/UI/UISelectable.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
 
 namespace UI
 {
-    using UnityEngine.Events;
-    using TMPro;
+    [System.Flags]
+    public enum UISelectableExtraClasses
+    {
+        NONE = 0,
+        BlockySlider = 0b0000_0001,
+        TextSwitcher = 0b0000_0010,
+        TextSwitcherMultiLanguage = 0b000_0100,
+        AnyTextSwitcher = TextSwitcher | TextSwitcherMultiLanguage,
+    }
     /// <summary>
     /// class for ui elements that are meant to be controlled by game pad
     /// and when you "click" on it something happens (ui elements meant to be controlled by game-pads)
     /// </summary>
-    public abstract class UISelectable 
+
+    public abstract class UISelectable : MonoBehaviour
     {
+        /// <summary>
+        /// Keeps track of which optional classes the instance of this type has
+        /// </summary>
+        public UISelectableExtraClasses extraClasses { get; private set; } = UISelectableExtraClasses.NONE;
+
+        [Header("base attributes")]
         /// <summary>
         /// This control the position, size and scale of the UI elements
         /// </summary>
@@ -24,6 +40,31 @@ namespace UI
         /// Text that is displayed to the User
         /// </summary>
         public TextMeshProUGUI textMeshProUGUI;
+
+        [Header("Optional extra classes")]
+        public BlockySlider blockySlider = null;
+        public TextSwitcher textSwitcher = null;
+        public TextSwitcherMultiLanguage textSwitcherMultiLanguage = null;
+
+        public void Awake()
+        {
+
+            if (blockySlider != null)
+            {
+                extraClasses |= UISelectableExtraClasses.BlockySlider;
+            }
+
+            if (textSwitcher != null)
+            {
+                extraClasses |= UISelectableExtraClasses.TextSwitcher;
+            }
+
+            if (textSwitcherMultiLanguage != null)
+            {
+                extraClasses |= UISelectableExtraClasses.TextSwitcherMultiLanguage;
+            }
+
+        }
 
 
         /// <summary>
@@ -43,6 +84,11 @@ namespace UI
         {
             RectTransform.SetParent(otherTransform);
             RectTransform.anchoredPosition = offSet;
+        }
+
+        public bool hasExtraClass(UISelectableExtraClasses mask)
+        {
+            return (extraClasses & mask) > 0;
         }
 
 
