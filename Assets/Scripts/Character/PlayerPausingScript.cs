dff366713fe5577da4e0a1392af553d9c7c957c4
y1z <whyis2cool@gmail.com>
2025-05-17 13:54:52 -0600

    PlayerPausingScript.cs : Script para hacer pausa

---
+++ Assets/Scripts/Character/PlayerPausingScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+namespace Character
+{
+
+    /// <summary>
+    /// This script let's the player invoke the pause menu
+    /// </summary>
+    public sealed class PlayerPausingScript : MonoBehaviour
+    {
+        private Input.Actions inputReceiver;
+        private GameStates currentGameState;
+
+        #region GameManagerBoilerPlate
+        private void OnEnable()
+        {
+            if (inputReceiver == null)
+            {
+                inputReceiver = GameManager.Instance.GetComponent<Input.Actions>();
+            }
+
+            GameManager.Instance.RegisterUnsubscribeAction(UnSubscribe);
+            GameManager.Instance.Subscribe(OnGameStateChange);
+            inputReceiver.OnPauseEvent += OnPause;
+        }
+
+        private void OnDisable()
+        {
+            inputReceiver.OnPauseEvent -= OnPause;
+        }
+
+
+        #endregion
+
+        #region EventMethods
+
+        private void OnPause(bool _)
+        {
+            if (currentGameState == GameStates.Playing)
+            {
+                GameManager.Instance.SetGameState(GameStates.Paused);
+            }
+            else if (currentGameState == GameStates.Paused)
+            {
+                GameManager.Instance.SetGameState(GameStates.Playing);
+            }
+        }
+
+        private void OnGameStateChange(GameStates _states)
+        {
+            this.currentGameState = _states;
+        }
+
+        private void UnSubscribe()
+        {
+            GameManager.Instance.Unsubscribe(OnGameStateChange);
+        }
+
+        #endregion
+
+
+    }
+
+}
