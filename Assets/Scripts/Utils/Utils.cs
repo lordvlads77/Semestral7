375de05c3201945ba312e7b17bb277a89b7b3dde
y1z <whyis2cool@gmail.com>
2025-05-23 15:50:36 -0600

    Utils.cs : Agregar util para remover un elemento de un arreglo

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Windows;
 using Random = UnityEngine.Random;
-
+using Color = UnityEngine.Color;
 using SCG = System.Collections.Generic;
 
 namespace Utils
@@ -573,6 +573,24 @@ namespace Utils
             return result;
         }
 
+    };
+
+
+
+    public static class Array<T>
+    {
+        public static void RemoveAt<T>(ref T[] arr, int index)
+        {
+            for (int a = index; a < arr.Length - 1; a++)
+            {
+                // moving elements downwards, to fill the gap at [index]
+                arr[a] = arr[a + 1];
+            }
+            // finally, let's decrement Array's size by one
+            Array.Resize(ref arr, arr.Length - 1);
+        }
     }
+
+
 
 }
