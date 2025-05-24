83acdc3ac655e4a9e3fbb60230e2e90d2901879c
y1z <whyis2cool@gmail.com>
2025-05-23 23:56:19 -0600

    CoroutineCaller.cs : Esto existe porque el SaveSystem no puede llamar coroutinas por si solo.

---
+++ Assets/Scripts/Save System/CoroutineCaller.cs
@@ -0,0 +1,11 @@
+namespace SaveSystem
+{
+    using Utils;
+    /// <summary>
+    /// All this class is used for is for non-mono-behavior to call coroutines
+    /// </summary>
+    public class CoroutineCaller : RegulatorSingleton<CoroutineCaller>
+    {
+    }
+
+}
