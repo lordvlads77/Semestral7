5c6ac7ee612183d38db275bcc6735b11fb9218ef
y1z <whyis2cool@gmail.com>
2025-05-26 18:03:16 -0600

    UI\SaveFileSelectablesInitScript.cs : Bugfix error de logica

--- Assets/Scripts/UI/SaveFileSelectablesInitScript.cs
+++ Assets/Scripts/UI/SaveFileSelectablesInitScript.cs
@@ -17,7 +17,7 @@ namespace UI
             saveFileSelectables = gameObject.GetComponentsInChildren<SaveFileSelectable>();
             Debug.Assert(saveFileSelectables != null, $"This script need to be put on the parent object of all the {typeof(SaveFileSelectable)} types", this);
             var curr_lang = LanguageManager.Instance.currentLanguage;
-            LanguageManager.Instance.setLanguage(curr_lang);
+            LanguageManager.Instance.ForceLanguageChange(curr_lang);
         }
 
         private void FixedUpdate()
@@ -25,10 +25,10 @@ namespace UI
 
             for (int i = 0; i < saveFileSelectables.Length; i++)
             {
-                bool doesSaveFileExist = SaveSystem.SaveSystem.DoesSaveFileExist(i);
+                bool isSaveBeingUsing = !SaveSystem.SaveSystem.isSaveFileEmpty(i);
 
                 saveFileSelectables[i].saveFileIndex = i;
-                saveFileSelectables[i].isBeingUsed = doesSaveFileExist;
+                saveFileSelectables[i].isBeingUsed = isSaveBeingUsing;
                 saveFileSelectables[i].setTextIndicator();
             }
             
